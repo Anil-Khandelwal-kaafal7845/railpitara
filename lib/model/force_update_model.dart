807916d8fe93f8879bd717832ea50d435c0a9121
rakshuUnvii <rakshit.mathpal@unvii.com>
2023-12-12 13:39:50 +0530

    updated  ios force update

--- lib/model/force_update_model.dart
+++ lib/model/force_update_model.dart
@@ -34,20 +34,32 @@ class ForceUpdatemodel {
 
 class Result {
     int? appVersion;
-    int? forceUpdate;
+    int? forceUpdateAndroid;
+    int? showPackage;
+    int? forceUpdateIos;
+    int? appVersionIos;
 
     Result({
         this.appVersion,
-        this.forceUpdate,
+        this.forceUpdateAndroid,
+        this.showPackage,
+        this.forceUpdateIos,
+        this.appVersionIos,
     });
 
     factory Result.fromJson(Map<String, dynamic> json) => Result(
         appVersion: json["app_version"],
-        forceUpdate: json["force_update"],
+        forceUpdateAndroid: json["force_update_android"],
+        showPackage: json["show_package"],
+        forceUpdateIos: json["force_update_ios"],
+        appVersionIos: json["app_version_ios"],
     );
 
     Map<String, dynamic> toJson() => {
         "app_version": appVersion,
-        "force_update": forceUpdate,
+        "force_update_android": forceUpdateAndroid,
+        "show_package": showPackage,
+        "force_update_ios": forceUpdateIos,
+        "app_version_ios": appVersionIos,
     };
 }
