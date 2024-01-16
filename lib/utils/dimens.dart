23872dc972207571fbf156191c35b1341418499f
rakshuUnvii <rakshit.mathpal@unvii.com>
2024-01-16 16:30:13 +0530

    updated height

--- lib/utils/dimens.dart
+++ lib/utils/dimens.dart
@@ -35,7 +35,7 @@ class Dimens {
   static double widthLangGen = 120;
   static double heightLangGen = 120;
   static double heightTopTen = 120;
-  static double widthTopTen = 150;
+  static double widthTopTen = 190;
   static double widthArtist = 100;
   static double heightArtist = 105;
     static double widthLangGenLand = 150;
