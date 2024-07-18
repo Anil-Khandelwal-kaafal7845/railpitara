e9288e4159e945d05fad7d2eee13cef79cd7a85e
Anil  khandelwal <anil.khandelwal@kaafal.com>
2024-07-18 19:19:29 +0530

    aab ready

--- lib/utils/constant.dart
+++ lib/utils/constant.dart
@@ -12,7 +12,7 @@ class Constant {
   static String appName = "Aaryaa digital";
   static String appPackageName = "com.release.aryanews";
   static String appleAppId = "1584477559";
-  static double curentAppVersion = 21;
+  static double curentAppVersion = 22;
 
   /* OneSignal App ID */
   static const String oneSignalAppId = "";
