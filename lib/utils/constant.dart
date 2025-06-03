b92a9adc4bf681ce77510959eeade62e25e9c2d4
Anil  khandelwal <anil.khandelwal@kaafal.com>
2025-06-03 12:10:01 +0530

    apk deploy done

--- lib/utils/constant.dart
+++ lib/utils/constant.dart
@@ -11,7 +11,7 @@ class Constant {
   static String appName = "Chull";
   static String appPackageName = "com.ott.chulbull";
   static String appleAppId = "";
-  static double curentAppVersion = 1;
+  static double curentAppVersion = 3;
   static dynamic curentiosAppVersion = 1;
 
   /* OneSignal App ID */
